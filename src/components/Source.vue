531ab7bfb17ebe1b14aeafa357c577229d320f28
lvyuanxin <lvyuanxin@wps.cn>
2022-11-29 17:46:17 +0800

    first commit

---
+++ src/components/Source.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="source">
+    <textarea name="" id="" class="source-input"></textarea>
+  </div>
+</template>
+
+<style scoped lang="less">
+.source {
+  height: 100%;
+  .source-input {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+  }
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Source from '../components/Source.vue';
+import EditorVue from '../components/Editor.vue';
+</script>
+
+<template>
+  <!-- <main class="main">
+    <div class="source-wrapper">
+      <Source />
+    </div>
+    <div class="transformed-wrapper">
+  
+    </div>
+  </main> -->
+  <EditorVue />
+</template>
+
+<style scoped lang="less">
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  .source-wrapper {
+    width: 48%;
+    height: 100%;
+    background-color: pink;
+  }
+  .transformed-wrapper {
+    width: 48%;
+    height: 100%;
+    background-color: bisque;
+  }
+}
+</style>
